92ed082c947a6949dce8711344c2519f5180671d
KennethSC <kennethsantacruz12@gmail.com>
2021-03-26 00:01:16 -0700

    Finished

--- app/blogengine/blog/utils.py
+++ app/blogengine/blog/utils.py
@@ -9,10 +9,8 @@ class DetailObjectMixin:
     
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
-        return render(request, self.template, context={self.model.__name__.lower(): obj})
-
-
-
+        return render(request, self.template, context={self.model.__name__.lower(): obj,
+        'admin_object': obj})
 
 
 class CreateObjectMixin:
@@ -30,4 +28,40 @@ class CreateObjectMixin:
             return redirect(new_obj)
 
         return render(request, self.template, context={'form': bound_form})
+
+
+class UpdateObjectMixin:
+    model = None
+    template = None
+    form_class = None
+
+    def get(self, request, slug):
+        obj = get_object_or_404(self.model, slug__iexact=slug)
+        bound_form = self.form_class(instance=obj)
+        return render(request, self.template, context={'form': bound_form,
+        self.model.__name__.lower(): obj}) 
+
+    def post(self, request, slug):
+        obj = get_object_or_404(self.model, slug__iexact=slug)
+        bound_form = self.form_class(request.POST, instance=obj)
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+
+        return render(request, self.template, context={'form': bound_form,
+        self.model.__name__.lower(): obj})
+
+class DeleteObjectMixin:
+    model = None
+    template = None
+    redirect_url = None
+
+    def get(self, request, slug):
+        obj = get_object_or_404(self.model, slug__iexact=slug)
         
+        return render(request, self.template, context={self.model.__name__.lower(): obj}) 
+
+    def post(self, request, slug):
+        obj = get_object_or_404(self.model, slug__iexact=slug)
+        obj.delete()
+        return redirect(reverse(self.redirect_url))
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import View
-
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .models import Post, Tag
 from .forms import TagForm, PostForm
-from .utils import DetailObjectMixin, CreateObjectMixin
+from .utils import DetailObjectMixin, CreateObjectMixin, UpdateObjectMixin, DeleteObjectMixin
 
 def posts_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/posts_list.html', context={'posts': posts})
 
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    if page.has_next():
+        next_url = f'?page={page.next_page_number()}'
+    else:
+        next_url = ''
+
+    if page.has_previous():
+        prev_url = f'?page={page.previous_page_number()}'
+    else:
+        prev_url = ''
+        
+    return render(request, 'blog/posts_list.html', context={'Paginator': Paginator, 'page': page,
+    'next_page_url': next_url, 'prev_page_url': prev_url})
 
 
 def tags_list(request):
@@ -28,13 +52,42 @@ class TagDetail(DetailObjectMixin, View):
     template = 'blog/tag_detail.html'
     
 
-class TagCreate(CreateObjectMixin, View):
+class TagCreate(LoginRequiredMixin, CreateObjectMixin, View):
     model = TagForm
     template = 'blog/tag_create.html' 
+    raise_exception = True
         
-
-
-class PostCreate(CreateObjectMixin, View):
+class PostCreate(LoginRequiredMixin, CreateObjectMixin, View):
     model = PostForm
-    template = 'blog/post_create.html' 
-    
+    template = 'blog/post_create.html'
+    raise_exception = True
+
+
+
+
+
+
+class TagUpdate(LoginRequiredMixin, UpdateObjectMixin, View):
+    model = Tag
+    template = 'blog/tag_update.html' 
+    form_class = TagForm
+    raise_exception = True
+
+class PostUpdate(LoginRequiredMixin, UpdateObjectMixin, View):
+    model = Post
+    template = 'blog/post_update.html' 
+    form_class = PostForm
+    raise_exception = True
+
+class TagDelete(LoginRequiredMixin, DeleteObjectMixin, View):
+    model = Tag
+    template = 'blog/tag_delete.html'
+    redirect_url = 'tags_list_url'
+    raise_exception = True
+
+
+class PostDelete(LoginRequiredMixin, DeleteObjectMixin, View):
+    model = Post
+    template = 'blog/post_delete.html'
+    redirect_url = 'posts_list_url'
+    raise_exception = True
